254db5160b1c3006b9607fce406337edfe67a7dd
HamsterKaget <radzaramdani21@gmail.com>
2022-03-07 20:19:28 +0700

    Deploying Code

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- AOS -->
+    <link rel="stylesheet" href="../../assets/vendor/aos/dist/aos.css">
+
+    
+    <!-- Font Awesome Kit -->
+    <script src="https://kit.fontawesome.com/38ab242903.js" crossorigin="anonymous"></script>
+
+    <!-- My Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2&display=swap" rel="stylesheet">
+    
+    <!-- Favicon -->
+    <link rel="icon" href="https://images-ext-1.discordapp.net/external/on70in54InrZ-UDV4jK0ezXBVFUqk4dNfYHIxt-spwk/https/cdn.discordapp.com/icons/878869835805761596/d685fbdc273690a919010e761eda0c00.png">
+    <title>HamsterKaget - LinkHub</title>
+
+
+</head>
+<body>
+    <nav>
+        <div class="navbar">
+            <div class="navbar-left">
+                <div class="container-navbar-left">
+                    <h4><a href="index.html">HamsterKaget Studio</a></h4>
+                    <p class="slogan">Making Stuff Free For Everyone</p>
+                </div>
+            </div>
+            <div class="navbar-right">
+                <div class="container-navbar-right">
+                    <ul>
+                        <li><a class="home" href="#">Home</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Invite Bot</a></li>
+                        <li><a href="#"><i class="fa-solid fa-circle-chevron-down fa-rotate-90"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="top">
+        <div class="top-wrapper">
+            <div class="avatar-hamster"></div>
+            <h1>HamsterKaget Studio</h1>
+            <div class="sosmed">
+                <a href="https://instagram.com/hamsterkageet" target="_blank" ><i class="fa-brands fa-instagram fa-2x"></i></a>
+                <a href="htpps://twitter.com/hamsterkageet" target="_blank"><i class="fa-brands fa-twitter fa-2x"></i></a>
+                <a href="https://dsc.gg/hamsterarea" target="_blank"><i class="fa-brands fa-discord fa-2x"></i></a>
+                <a href="https://github.com/hamsterkaget" target="_blank"><i class="fa-brands fa-github fa-2x"></i></a>
+                <a href="https://youtube.com/c/HamsterkagetStudio" target="_blank"><i class="fa-brands fa-youtube fa-2x"></i></a>
+                <a href="https://id.linkedin.com/in/radja-aulia-al-ramdani-595723228" target="_blank"><i class="fa-brands fa-linkedin fa-2x"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="cards">
+        <div data-aos="fade-right" class="card a">
+            <h4><a href="#">HamsShop</a></h4>
+        </div>
+        <div data-aos="fade-left" class="card b">
+            <h4><a href="#">Hamster Area</a></h4>
+        </div>
+        <div data-aos="fade-right" class="card d">
+            <h4><a href="#">Hams Bot</a></h4>
+        </div>
+        <div data-aos="fade-left" class="card c">
+            <h4><a href="#">Discord Store ID</a></h4>
+        </div>
+    </div>
+
+    <script src="../../assets/vendor/aos/dist/aos.js"></script>
+
+    <script>
+        $(document).on('ready', function () {
+            AOS.init();
+        });
+    </script>
+
+
+</body>
+</html>
